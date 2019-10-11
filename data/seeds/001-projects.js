718fcb90113f49ddbeefdef517b9cd486d60ebfc
zachary-peasley <zpman95@hotmail.com>
2019-10-11 11:30:08 -0500

    project seed

--- data/seeds/001-projects.js
+++ data/seeds/001-projects.js
@@ -4,10 +4,17 @@ exports.seed = function(knex) {
   return knex('table_name').del()
     .then(function () {
       // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+      return knex('projects').insert([
+        {
+            name: 'USA somthing',
+            description: 'Track your projects',
+            completed: false
+        },
+        {
+            name: 'Rehab',
+            description: 'Say not to drugs',
+            completed: true
+        }
       ]);
     });
 };
