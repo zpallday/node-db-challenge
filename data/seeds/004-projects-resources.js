f13f00f98dfee5b35773fc5c0bc8cc6ae80301c0
zachary-peasley <zpman95@hotmail.com>
2019-10-11 11:39:10 -0500

    project res

--- data/seeds/004-projects-resources.js
+++ data/seeds/004-projects-resources.js
@@ -1,13 +1,22 @@
 
 exports.seed = function(knex) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('projects-resources').del()
     .then(function () {
       // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+      return knex('projects-resources').insert([
+       {
+        project_id: 1,
+        resource_id: 1
+       },
+      {
+       project_id: 1,
+       resource_id: 2
+      },
+     {
+      project_id: 2,
+      resource_id: 3
+      }
       ]);
     });
 };
