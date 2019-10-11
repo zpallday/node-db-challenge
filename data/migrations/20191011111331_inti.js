5daf931793760753991374e24530dd33a174df57
zachary-peasley <zpman95@hotmail.com>
2019-10-11 11:25:21 -0500

    migrations typed

--- data/migrations/20191011111331_inti.js
+++ data/migrations/20191011111331_inti.js
@@ -1,8 +1,40 @@
 
 exports.up = function(knex) {
-  
+  return (knex.schem .createTable('projects', (tbl) => {
+      tbl.increments('id').primary();
+      tbl.string('name').notNullable();
+      tbl.string('description')
+      tbl.boolean('completed').notNullable();
+  })
+  .createTable('tasks', (tbl) => {
+      tbl.increments('id').primary();
+      tbl.string('description').notNullable();
+      tbl.string('notes')
+      tbl.boolean('completed').notNullable();
+      tbl.integer('project_id').notNullable().references('projects.id');
+  })
+.createTable('resources', (tbl) => {
+    tbl.increments('id').primary();
+    tbl.string('name').unique().notNullable();
+    tbl.string('description');
+})
+   .createTable('projects-resources', (tbl) => {
+       tbl.integer('project_id').notNullable().references();
+       tbl.integer('resouce_id').notNullable().references();
+   })
+
+
+
+  )
 };
 
+
 exports.down = function(knex) {
-  
+  return (
+      knex.schema
+      .dropTableIfExists('projects-resources')
+      .dropTableIfExists('tasks')
+      .dropTableIfExists('resources')
+      .dropTableIfExists('projects')
+  )
 };
